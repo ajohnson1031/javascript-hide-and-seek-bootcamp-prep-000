3a9591e630f4e8edad8a9e7d137d4384e87e2e99
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-08 12:12:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function getFirstSelector(x) {
   return document.querySelector(x)
 }
 
-function nestedTarget(n, t) {
-  let nested = n
-  let target = t
-  return document.getElementById(`${nested}`).querySelector(`.${target}`)
+function nestedTarget() {
+  return document.getElementById('#nested').querySelector('.target')
 }
