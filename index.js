4b026326858d08c8ee807744a783c122814cd1a2
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-08 12:30:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(x) {
+  return document.querySelector(x)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const targ = document.querySelectorAll('.ranked-list li')
+
+  for(let i in targ) {
+    let x = parseInt(targ[i].innerHTML) + n
+    targ[i].innerHTML = x
+  }
+
+}
+
+function deepestChild() {
+  const n = document.querySelectorAll('#grand-node div')
+  const l = n.length
+
+  return n[l - 1]
+}
